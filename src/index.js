2fcff0a7888924360051b3fa51fccbd7490eb4fe
Sadioho <hoxuananh9.3@gmail.com>
2021-09-02 21:38:14 +0700

    setup MVC

--- src/index.js
+++ src/index.js
@@ -1,49 +1,37 @@
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+const db = require('./config/db');
+
+//connect db
+db.connect();
+
 // file static
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //middle ware
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//HTTP logger
-app.get("/", function (req, res) {
-  res.render("home");
-});
-
-app.get("/news", function (req, res) {
-  res.render("news");
-});
-
-app.get("/search", function (req, res) {
-  // console.log(req.query.q);
-  res.render("search");
-});
-
-app.post("/search", function (req, res) {
-  console.log(req.body);
-  res.send("");
-});
+route(app);
 
 //template engine
 app.engine(
-  "hbs",
+  'hbs',
   handlebars({
-    extname: ".hbs",
+    extname: '.hbs',
   })
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //morgan
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
